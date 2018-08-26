d30db393823031f786fb749c2825bb4b654f240a
Jake Lauer <jake.lauer27@gmail.com>
2018-08-26 14:07:49 -0600

    Add function for creating new HTML Element

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 /////GLOBAL VARS
 
-var enterButton = document.querySelector("enter-button");
-var websiteTitle = document.querySelector("website-title");
-var websiteURL = document.querySelector("website-url");
+var enterButton = document.querySelector(".enter-button");
+var websiteTitle = document.querySelector(".website-title");
+var websiteURL = document.querySelector(".website-url");
+var results = document.querySelector(".results");
 
 var linkCount = 0;
 var readLinks = 0;
 var unreadLinks = 0;
 
 enterButton.addEventListener("click", function() {
-  validateInput();
+  createHTML();
 });
 
 //////CONTRUCTOR FUNCTION
 
-function WebsiteItem(title, url) {
-  this.title = title;
-  this.url = url;
-}
+/////CREATEHTMLFUNCTION
 
-WebsiteItem.prototype.close = function(){}
+function createHTML(){
+  var displayTitle = websiteTitle.value;
+  var displayWebAddress = websiteURL.value;
+  results.insertAdjacentHTML("beforeend", 
+  `<article class ="bookmark">
+  <h2>${displayTitle}</h2>
+  <hr>
+  <a class="website-link" href="${displayWebAddress}">${displayWebAddress}</a>
+  <hr>
+  <button class="read-button">Read</button>
+  <button class="delete-button">Delete</button>
+ </article>`)
+}
 
 /////HELPER FUNCTIONS
 
